0f4df94a69cadcd03767498c6822ef6eb6572efc
Martin Morgan <Martin.Morgan@RoswellPark.org>
2023-06-15 16:28:12 -0300

    reorder for second workshop

---
+++ R/xm2023_cxg_dataset.R
@@ -0,0 +1,110 @@
+#' @rdname xm2023_cxg_dataset
+#'
+#' @title Summarize CELLxGENE Information About a Dataset
+#'
+#' @param dataset_id character(1) dataset identifier, as returned by,
+#'     e.g., `datasets()`.
+#'
+#' @importFrom dplyr filter .data
+#'
+#' @importFrom rjsoncons jmespath
+#'
+#' @importFrom jsonlite toJSON fromJSON
+#'
+#' @importFrom cellxgenedp db datasets collections
+#'
+#' @examples
+#' dataset <- "24205601-0780-4bf2-b1d9-0e3cacbc2cd6"
+#' ds <- xm2023_cxg_dataset(dataset)
+#' ds
+#'
+#' @export
+xm2023_cxg_dataset <-
+    function(dataset_id)
+{
+    ok <- tryCatch({
+        curl::nslookup("api.cellxgene.cziscience.com")
+        TRUE
+    }, error = function(...) {
+        FALSE
+    })
+    if (!ok) {
+        return("internet not available")
+    }
+    id <- dataset_id
+    cellxgene_db = db()
+
+    ## retrieve dataset and collection information
+    dataset <-
+        datasets(cellxgene_db) |>
+        filter(.data$dataset_id == id)
+    collection <-
+        collections(cellxgene_db) |>
+        filter(.data$collection_id == dataset$collection_id)
+
+    ## create an object with relevant information
+    json <- jsonlite::toJSON(collection, auto_unbox = TRUE)
+    family <-
+        jmespath(json, "[].publisher_metadata[].authors[].family") |>
+        fromJSON()
+    given <- 
+        jmespath(json, "[].publisher_metadata[].authors[].given") |>
+        fromJSON()
+    authors <- paste(family, given, sep = ", ", collapse = "; ")
+    journal <-
+        jmespath(json, "[].publisher_metadata[].journal") |>
+        fromJSON()
+
+    dataset_assay <-
+        jmespath(dataset, "[].assay[].label[]") |>
+        fromJSON() |>
+        paste(collapse = "; ")
+    dataset_organism <-
+        jmespath(dataset, "[].organism[].label[]") |>
+        fromJSON() |>
+        paste(collapse = "; ")
+    dataset_ethnicity <-
+        jmespath(dataset, "[].self_reported_ethnicity[].label[]") |>
+        fromJSON() |>
+        paste(collapse = "; ")
+    structure(list(
+        collection_name = collection$name,
+        collection_description = collection$description,
+        collection_authors = authors,
+        collection_journal = journal,
+        dataset_assay = dataset_assay,
+        dataset_organism = dataset_organism,
+        dataset_ethnicity = dataset_ethnicity
+    ), class = "cxg_dataset")
+}
+
+
+.pretty <-
+    function(label, ..., indent = 0, exdent = 4)
+{
+    x <- paste(label, paste0(...), sep = ": ")
+    paste(strwrap(x, indent = indent, exdent = exdent), collapse = "\n")
+}
+
+#' @rdname xm2023_cxg_dataset
+#'
+#' @param x A `cxg_dataset` object resulting from a call to
+#'     `training_cxg_dataset()`.
+#'
+#' @param ... Additional arguments (to `print.cxg_dataset()`); ignored.
+#'
+#' @export
+print.cxg_dataset <-
+    function(x, ...)
+{
+    cat(
+        .pretty("title", x$collection_name), "\n",
+        .pretty("description", x$collection_description), "\n",
+        .pretty("authors", x$collection_authors), "\n",
+        .pretty("journal", x$collection_journal), "\n",
+        .pretty("assays", x$dataset_assay), "\n",
+        .pretty("organism", x$dataset_organism), "\n",
+        .pretty("ethnicity", x$dataset_ethnicity), "\n",
+        sep = ""
+    )
+}
